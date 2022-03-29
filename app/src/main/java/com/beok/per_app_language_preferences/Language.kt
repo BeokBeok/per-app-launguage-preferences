c91a639c46eb0cc3c4ce71a9fd21ee24035747af
BeokBeok <kekemusa37@gmail.com>
2022-03-29 23:05:43 +0900

    :sparkles: [적용] per-app language preferences

---
+++ app/src/main/java/com/beok/per_app_language_preferences/Language.kt
@@ -0,0 +1,5 @@
+package com.beok.per_app_language_preferences
+
+enum class Language(val value: String) {
+    NONE(""), US("en-US"), ES("es-ES"), IL("iw-IL"), KR("ko-KR"), UA("uk-UA")
+}
--- app/src/main/java/com/beok/per_app_language_preferences/MainActivity.kt
+++ app/src/main/java/com/beok/per_app_language_preferences/MainActivity.kt
@@ -1,43 +1,94 @@
 package com.beok.per_app_language_preferences
 
+import android.app.LocaleManager
 import android.os.Bundle
+import android.os.LocaleList
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import com.beok.per_app_language_preferences.ui.theme.PerapplanguagepreferencesTheme
+import java.util.Locale
 
+@RequiresApi(33)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PerapplanguagepreferencesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            PerAppLanguagePreferencesTheme()
+        }
+    }
+
+    @Composable
+    private fun PerAppLanguagePreferencesTheme() {
+        PerapplanguagepreferencesTheme {
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+                PerAppLanguageDropBoxMenu()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    private fun PerAppLanguageDropBoxMenu() {
+        var isDropDownMenuExpanded by remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PerapplanguagepreferencesTheme {
-        Greeting("Android")
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier.wrapContentSize()) {
+                Button(
+                    onClick = { isDropDownMenuExpanded = true },
+                    modifier = Modifier.wrapContentSize()
+                ) {
+                    Text(text = currentLanguage())
+                }
+
+                DropdownMenu(
+                    expanded = isDropDownMenuExpanded,
+                    onDismissRequest = { isDropDownMenuExpanded = false },
+                ) {
+                    Language.values().forEach {
+                        DropdownMenuItem(
+                            onClick = {
+                                updateAppLocale(Locale.forLanguageTag(it.value))
+                                isDropDownMenuExpanded = false
+                            }
+                        ) {
+                            Text(text = it.value)
+                        }
+                    }
+                }
+            }
+            Text(text = stringResource(id = R.string.hello_android))
+        }
+    }
+
+    private fun updateAppLocale(vararg locales: Locale) {
+        val localeManager = getSystemService(LocaleManager::class.java)
+        localeManager.applicationLocales = LocaleList(*locales)
+    }
+
+    private fun currentLanguage(): String {
+        val localeManager = getSystemService(LocaleManager::class.java)
+        val appLocales = localeManager.applicationLocales
+        return if (appLocales.isEmpty) "" else appLocales[0].displayName
     }
 }
